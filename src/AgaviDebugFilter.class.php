a747d45e8c3ebdb2f9aab6c352097f50b3c6263a
whisller <whisller@4fb8c668-4752-0410-9dd1-864e37ccdd3b>
2008-07-21 11:24:56 +0000

    Re-write sync-modpub.php

--- src/AgaviDebugFilter.class.php
+++ src/AgaviDebugFilter.class.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * AgaviDebugFilter gathers information for debug purposes
  *
@@ -11,89 +10,89 @@
 class AgaviDebugFilter extends AgaviFilter implements AgaviIActionFilter
 {
 
-	protected $log = array();
+  protected $log = array();
   /**
    * @var AgaviExecutionContainer
    */
   private $container = null;
 
-	public function executeOnce(AgaviFilterChain $filterChain, AgaviExecutionContainer $container)
-	{
-	  $this->container = $container;
-	  
-		$this->context->getLoggerManager()->log(__CLASS__.' executeOnce', 'debug');
-		$this->execute($filterChain, $container);
-		
-		$this->log['routes']       = $this->getMatchedRoutes();
-		$this->log['request_data'] = $this->getContext()->getRequest()->getRequestData()->getParameters();
-		$this->log['view']         = $this->adtGetViewHtml();
-		$this->log['log']          = $this->getLogLines();
-		$this->log['database']     = $this->adtGetDatabase();
-		
-	}
+  public function executeOnce(AgaviFilterChain $filterChain, AgaviExecutionContainer $container)
+  {
+    $this->container = $container;
 
-	public function execute(AgaviFilterChain $filterChain, AgaviExecutionContainer $container)
-	{
-		$this->context->getLoggerManager()->log(__CLASS__.' execute', 'debug');
-		
-		//procede
-		$filterChain->execute($container);
-		//log what can be logged.
-		$this->log($container);
-	}
-	
-	protected function log(AgaviExecutionContainer $container)
-	{
-		//keep this simple for now
-		$this->log['actions'][] = array (
-			'name'         => $container->getActionName(),
-			'module'       => $container->getModuleName(),
-			'request_data' => $container->getRequestData(),
-			'view'         => $this->adtGetViewHtml()
-		);
-	}
-	
-	/**
-	 * Get array with matched routes
-	 *
-	 * @return array
-	 * @since 0.1
-	 */
-	private function getMatchedRoutes() {
-		# Array with information about matched routes, name of route is an index of array
-		$matchedRoutesInformation = array();
-		# Matched routes
-		$matchedRoutes = $this->getContext()->getRequest()->getAttribute('matched_routes', 'org.agavi.routing');
+    $this->context->getLoggerManager()->log(__CLASS__.' executeOnce', 'debug');
+    $this->execute($filterChain, $container);
 
-		foreach( $matchedRoutes as $matchedRoute ) {
-			$matchedRoutesInformation[$matchedRoute] = $this->getContext()->getRouting()->getRoute($matchedRoute);
-		}
+    $this->log['routes']       = $this->getMatchedRoutes();
+    $this->log['request_data'] = $this->getContext()->getRequest()->getRequestData()->getParameters();
+    $this->log['view']         = $this->adtGetViewHtml();
+    $this->log['log']          = $this->getLogLines();
+    $this->log['database']     = $this->adtGetDatabase();
 
-		return $matchedRoutesInformation;
-	}
-	
-	/**
-	 * Get informations about database
-	 * 
-	 * @since 0.1
-	 * @todo 
-	 * Integration with Propel logger ( http://propel.phpdb.org/trac/wiki/Users/Documentation/1.3/ConfigureLogging ) for 1.3 version
-	 */
-	private function adtGetDatabase() {
-	  $database = array();
-	  if ( !AgaviConfig::get('core.use_database') ) {
-	    return $database;
-	  }
-	  
-	  $database['class_name'] = get_class($this->container->getContext()->getDatabaseManager()->getDatabase());
-	  
-	  return $database;
-	}
+  }
 
-	public function getLogLines()
-	{
-		return $this->context->getRequest()->getAttribute('log', 'debugtoolbar', array());
-	}
+  public function execute(AgaviFilterChain $filterChain, AgaviExecutionContainer $container)
+  {
+    $this->context->getLoggerManager()->log(__CLASS__.' execute', 'debug');
+
+    //procede
+    $filterChain->execute($container);
+    //log what can be logged.
+    $this->log($container);
+  }
+
+  protected function log(AgaviExecutionContainer $container)
+  {
+    //keep this simple for now
+    $this->log['actions'][] = array (
+      'name'         => $container->getActionName(),
+      'module'       => $container->getModuleName(),
+      'request_data' => $container->getRequestData(),
+      'view'         => $this->adtGetViewHtml()
+    );
+  }
+
+  /**
+   * Get array with matched routes
+   *
+   * @return array
+   * @since 0.1
+   */
+  private function getMatchedRoutes() {
+    # Array with information about matched routes, name of route is an index of array
+    $matchedRoutesInformation = array();
+    # Matched routes
+    $matchedRoutes = $this->getContext()->getRequest()->getAttribute('matched_routes', 'org.agavi.routing');
+
+    foreach( $matchedRoutes as $matchedRoute ) {
+      $matchedRoutesInformation[$matchedRoute] = $this->getContext()->getRouting()->getRoute($matchedRoute);
+    }
+
+    return $matchedRoutesInformation;
+  }
+
+  /**
+   * Get informations about database
+   *
+   * @since 0.1
+   * @todo
+   * Integration with Propel logger ( http://propel.phpdb.org/trac/wiki/Users/Documentation/1.3/ConfigureLogging ) for 1.3 version
+   */
+  private function adtGetDatabase() {
+    $database = array();
+    if ( !AgaviConfig::get('core.use_database') ) {
+      return $database;
+    }
+
+    $database['class_name'] = get_class($this->container->getContext()->getDatabaseManager()->getDatabase());
+
+    return $database;
+  }
+
+  public function getLogLines()
+  {
+    return $this->context->getRequest()->getAttribute('log', 'debugtoolbar', array());
+  }
 
 }
 ?>
--- sync-modpub.php
+++ sync-modpub.php
@@ -1,7 +1,56 @@
 <?php
+/**
+ * Copy and paste all needed files from DEV to Demo - for modpub directory
+ *
+ * @author    Daniel Ancuta <daniel.ancuta@whisnet.pl>
+ * @author    Veikko Mäkinen <veikko@veikko.fi>
+ * @copyright Authors
+ * @version   0.1
+ */
 
-//copy modpub stuff into demo app's modpup
-//TODO: use PHP, not DOS :)
-exec('xcopy /E /Y modpub demo\pub\modpub\\');
+/**
+ * Paths
+ */
+# Demo
+$demoModpubPath = dirname(__FILE__).'/demo/pub/modpub';
+# Dev
+$modpubPath     = dirname(__FILE__).'/modpub';
 
+# Remove all files and directories from demo
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($demoModpubPath), RecursiveIteratorIterator::CHILD_FIRST);
+
+for(; $iterator->valid(); $iterator->next()) {
+  $rdi = $iterator->getInnerIterator();
+
+  if ( (strpos( $rdi->getSubpathname(), '.svn' )!==false) || $iterator->isDot() ) {
+    continue;
+  }
+
+  if ( $rdi->isFile() ) {
+    unlink( $rdi->getPathname() );
+  } else {
+    rmdir( $rdi->getPathname() );
+  }
+}
+
+# Create our ADT dir in modpub :)
+mkdir("./demo/pub/modpub/AgaviDebugToolbar");
+
+
+# Copy and paste all needed files from dev to demo
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($modpubPath), RecursiveIteratorIterator::SELF_FIRST);
+
+for(; $iterator->valid(); $iterator->next()) {
+  $rdi = $iterator->getInnerIterator();
+
+  if ( (strpos( $rdi->getSubpathname(), '.svn' )!==false) || $iterator->isDot() ) {
+    continue;
+  }
+
+  if ( $rdi->isDir() ) {
+    mkdir('./demo/pub/modpub/AgaviDebugToolbar/'.$rdi->getSubPathname());
+  } else {
+    copy( $rdi->getPathname(), './demo/pub/modpub/AgaviDebugToolbar/'.$rdi->getSubPathname() );
+  }
+}
 ?>
