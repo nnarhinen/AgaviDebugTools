b3034ee05d56dd53b46ac75f1bb45eff960889d5
v-dogg <v-dogg@4fb8c668-4752-0410-9dd1-864e37ccdd3b>
2008-11-07 15:20:02 +0000

    html template refactorings

--- src/templates/html.php
+++ src/templates/html.php
@@ -1,201 +1,21 @@
 
 <div id="adt-container">
-	<h1>Debug Log</h1>
+	<h1>ADT Debug Toolbar</h1>
 
 	<div id="sections">
 		<h2>Matched Routes</h2>
 		<div id="section-routing">
-
-			<?php foreach($template['routes'] as $routeName => $routeInfo): ?>
-			<h3><?php echo $routeName; ?></h3>
-			<div>
-				<dl>
-					<dt>Module:</dt>
-						<dd><?php echo !empty($routeInfo['opt']['module']) ? $routeInfo['opt']['module'] : '&#160;'; ?></dd>
-					<dt>Action:</dt>
-						<dd><?php echo !empty($routeInfo['opt']['action']) ? $routeInfo['opt']['action'] : '&#160;'; ?></dd>
-					<dt>Stop:</dt>
-						<dd><?php echo $routeInfo['opt']['stop']==true ? 'True' : 'False'; ?></dd>
-					<dt>Parameters:</dt>
-					<dd>
-						<?php if(count($routeInfo['par'])): ?>
-						<ul>
-						<?php foreach( $routeInfo['par'] as $oneParameter ): ?>
-							<li><strong style="text-decoration: underline;"><?php echo $oneParameter ?></strong><br />
-							Default:
-							<?php if ( isset($routeInfo['opt']['defaults'][$oneParameter]) ): ?>
-							<ul>
-								<li>Pre: <?php echo $routeInfo['opt']['defaults'][$oneParameter]['pre']; ?></li>
-								<li>Value: <?php echo $routeInfo['opt']['defaults'][$oneParameter]['val']; ?></li>
-								<li>Post: <?php echo $routeInfo['opt']['defaults'][$oneParameter]['post']; ?></li>
-							</ul>
-							<?php else: ?>
-							No default
-							<?php endif; ?>
-							<br />
-							Required: <?php echo isset($routeInfo['opt']['optional_parameters'][$oneParameter]) ? 'False' : 'True'; ?>
-							</li>
-						<?php endforeach; ?>
-						</ul>
-						<?php else: ?>
-						No Parameters
-						<?php endif; ?>
-					</dd>
-
-					<?php
-						$otTimeName = time()+rand();
-						$otName = empty($routeInfo['opt']['output_type']) ? $this->getContext()->getController()->getOutputType()->getName() : $routeInfo['opt']['output_type'];
-						$outputType = $this->getContext()->getController()->getOutputType($otName);
-					?>
-					<dt>Output type: <a id="adtMatchedRouteOutputTypeShow_<?php echo $otTimeName; ?>" href="#"><?php echo $otName;	?></a></dt>
-					<dd>
-					<div id="adtMatchedRouteOutputTypeInfo_<?php echo $otTimeName; ?>" >
-						Has renderers:	<?php echo $outputType->hasRenderers()==true ? 'True' : 'False'; ?><br />
-						Default layout name: <?php echo $outputType->getDefaultLayoutName(); ?>
-					</div>
-					<?php
-					if ( strcmp($this->getContext()->getController()->getOutputType()->getName(), $routeInfo['opt']['output_type']) == 0 ) {
-						echo ' ( default ) ';
-					}
-					?>
-					</dd>
-
-					<dt>Ignores:</dt>
-					<dd>
-					<?php if ( count( $routeInfo['opt']['ignores'] ) != 0 ): ?>
-					<ul>
-					<?php foreach( $routeInfo['opt']['ignores'] as $ignore ): ?>
-						<li><?php echo $ignore; ?></li>
-					<?php endforeach; ?>
-					</ul>
-					<?php else: ?>
-						No ignores
-					<?php endif; ?>
-					</dd>
-
-					<dt>Children:</dt>
-					<dd>
-					<?php if (count($routeInfo['opt']['childs']) != null ): ?>
-						<ul>
-						<?php foreach( $routeInfo['opt']['childs'] as $children ): ?>
-							<li><?php echo $children; ?></li>
-						<?php endforeach; ?>
-						</ul>
-					<?php else: ?>
-					No children
-					<?php endif; ?>
-					</dd>
-
-					<dt>Callback:</dt>
-					<dd>
-					<?php echo !empty( $routeInfo['opt']['callback'] )?$routeInfo['opt']['callback']:' No callback '; ?>
-					</dd>
-
-					<dt>Imply:</dt>
-					<dd>
-					<?php echo $routeInfo['opt']['imply']==true?'True':'False'; ?>
-					</dd>
-
-					<dt>Cut:</dt>
-					<dd>
-					<?php echo $routeInfo['opt']['cut']==true?'True':'False'; ?>
-					</dd>
-
-					<dt>rpx:</dt>
-					<dd>
-					<?php echo htmlspecialchars($routeInfo['rxp']); ?>
-					</dd>
-				</dl>
-			</div><!-- route -->
-			<?php endforeach; //routes ?>
+			<?php include('html-routing.php'); ?>			
 		</div><!-- routing -->
 
 		<h2>Global Request Data</h2>
 		<div id="section-globalrd" >
-			<h3>Request parameters</h3>
-			<dl>
-			<?php foreach( $template['request_data']['request_parameters'] as $parameter => $value ): ?>
-				<dt><?php echo $parameter; ?></dt>
-				<dd><pre><?php htmlspecialchars(var_dump($value)); ?></pre></dd>
-			<?php endforeach; ?>
-			</dl>
-
-			<h3>Cookies</h3>
-			<dl>
-			<?php foreach( $template['request_data']['cookies'] as $parameter => $value ): ?>
-				<dt><?php echo $parameter; ?></dt>
-				<dd><pre><?php htmlspecialchars(var_dump($value)); ?></pre></dd>
-			<?php endforeach; ?>
-			</dl>
-
-			<h3>Headers</h3>
-			<dl>
-			<?php foreach( $template['request_data']['headers'] as $parameter => $value ): ?>
-			<dt><?php echo $parameter; ?></dt>
-			<dd><?php echo htmlspecialchars($value); ?></dd>
-			<?php endforeach; ?>
-			</dl>
+			<?php include('html-globalrd.php'); ?>
 		</div>
 
 		<h2>Actions</h2>
 		<div id="section-actions" >
-			<?php foreach($template['actions'] as $action): ?>
-			<h3><?php echo $action['module']; ?>.<?php echo $action['name']; ?></h3>
-			<div>
-				View name: <?php echo $action['view']['view_name']; ?>
-				<br />
-				Output type: <a href="#"><?php echo $action['view']['output_type']; ?></a>
-				<?php if ( strcmp($action['view']['output_type'], $action['view']['default_output_type']) == 0 ): ?>
-				( default )
-				<?php endif; ?>
-				<br />
-				Has renderers: <?php echo $action['view']['has_renders']?'True':'False'; ?>
-				<br />
-				Default layout name: <?php echo $action['view']['default_layout_name']; ?>
-				
-				<h4>Validation</h4>
-				<div>
-					Has validation errors: <?php var_export($action['validation']['has_errors']); ?>
-					<?php if(count($action['validation']['incidents'])): ?>
-						<h5>Validation Incidents</h5>
-						<div>
-						<?php foreach($action['validation']['incidents'] as $incident): /* @var $incident AgaviValidationIncident */ ?>
-							Validator: <?php echo $incident->getValidator()->getName() ?><br/>
-							Severity: <?php echo $action['validation']['severities'][$incident->getSeverity()] ?><br/>							
-							Field(s): <?php echo implode(', ', $incident->getFields()); ?><br/>							
-						<?php endforeach; ?>
-						</div>
-					<?php endif; ?>
-				</div>
-				<h4>Request Data (from execution container)</h4>
-				<div>
-					<h5>Request parameters</h5>
-					<dl>
-					<?php foreach( $action['request_data']['request_parameters'] as $parameter => $value ): ?>
-						<dt><?php echo $parameter; ?></dt>
-						<dd><pre><?php htmlspecialchars(var_dump($value)); ?></pre></dd>
-					<?php endforeach; ?>
-					</dl>
-		
-					<h5>Cookies</h5>
-					<dl>
-					<?php foreach( $action['request_data']['cookies'] as $parameter => $value ): ?>
-						<dt><?php echo $parameter; ?></dt>
-						<dd><pre><?php htmlspecialchars(var_dump($value)); ?></pre></dd>
-					<?php endforeach; ?>
-					</dl>
-		
-					<h5>Headers</h5>
-					<dl>
-					<?php foreach( $action['request_data']['headers'] as $parameter => $value ): ?>
-					<dt><?php echo $parameter; ?></dt>
-					<dd><?php echo htmlspecialchars($value); ?></dd>
-					<?php endforeach; ?>
-					</dl>
-				</div>
-				
-			</div>
-			<?php endforeach; ?>
+			<?php include('html-actions.php'); ?>
 		</div>
 
 		<h2>FPF</h2>
